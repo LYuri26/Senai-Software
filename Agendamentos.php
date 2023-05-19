ed35dd4e86f9d8e12cb537c0c77ec9227a521ba6
grupoSenai23 <senaistartup@gmail.com>
2023-05-19 10:28:53 -0300

    Senai Software v1.0.7
    Atualizações e modificações nos códigos do software. Correções de bugs e erros.
    
    Co-Authored-By: João Victor Martins Deamo <57118631+BatataRaiz@users.noreply.github.com>

--- Agendamentos.php
+++ Agendamentos.php
@@ -4,7 +4,7 @@ session_start();
 // Verificar se o usuário está autenticado
 if (!isset($_SESSION['usuario'])) {
     // Usuário não está autenticado, redirecionar para a página de login
-    header('Location: Login.php');
+    header('Location: login.html');
     exit;
 }
 ?>
@@ -21,55 +21,65 @@ if (!isset($_SESSION['usuario'])) {
 
 <body>
     <a href="logout.php" class="sair">Sair</a>
-    <h1>Lista de Agendamentos</h1>
-    <?php
-    // Definir as informações de conexão
-    $host = 'localhost';
-    $banco = 'biblioteca';
-    $usuario = 'root';
-    $senha = '';
 
-    // Conectar ao banco de dados usando mysqli
+    <div id="app">
+        <form action="3.relacionar.php.php" method="post" onsubmit="exibirAlerta(event)">
+            <h1>Lista de Agendamentos</h1>
+            <?php
+            // Definir as informações de conexão
+            $host = 'localhost';
+            $banco = 'biblioteca';
+            $usuario = 'root';
+            $senha = '';
 
-    try {
-        $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8mb4", $usuario, $senha);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Conexão falhou" . $e->getMessage();
-    }
+            // Conectar ao banco de dados usando mysqli
 
-    // buscar agendamentos no banco de dados
-    $query = "SELECT * FROM agendamentos";
-    $busca = $pdo->query($query);
-    $agendamentos = $busca->fetchAll(PDO::FETCH_ASSOC);
-    ?>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Data</th>
-                <th>Hora de Início</th>
-                <th>Hora de Término</th>
-                <th>Quantidade de Alunos</th>
-                <th>Curso</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($agendamentos as $agendamento) : ?>
-                <tr>
-                    <td><?php echo $agendamento['id']; ?></td>
-                    <td><?php echo $agendamento['nome']; ?></td>
-                    <td><?php echo $agendamento['data']; ?></td>
-                    <td><?php echo $agendamento['hora_inicio']; ?></td>
-                    <td><?php echo $agendamento['hora_termino']; ?></td>
-                    <td><?php echo $agendamento['quantidade_alunos']; ?></td>
-                    <td><?php echo $agendamento['curso']; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    <script src="./config/assets/js/destruirSessao.js"></script>
+            try {
+                $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8mb4", $usuario, $senha);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo "Conexão falhou" . $e->getMessage();
+            }
+
+            // buscar agendamentos no banco de dados
+            $query = "SELECT * FROM agendamentos";
+            $busca = $pdo->query($query);
+            $agendamentos = $busca->fetchAll(PDO::FETCH_ASSOC);
+            ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Data</th>
+                        <th>Hora de Início</th>
+                        <th>Hora de Término</th>
+                        <th>Quantidade de Alunos</th>
+                        <th>Curso</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($agendamentos as $agendamento) : ?>
+                        <tr>
+                            <td><?php echo $agendamento['id']; ?></td>
+                            <td><?php echo $agendamento['nome']; ?></td>
+                            <td><?php echo $agendamento['data']; ?></td>
+                            <td><?php echo $agendamento['hora_inicio']; ?></td>
+                            <td><?php echo $agendamento['hora_termino']; ?></td>
+                            <td><?php echo $agendamento['quantidade_alunos']; ?></td>
+                            <td><?php echo $agendamento['curso']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+                    </div>
+            <footer>
+                <div class="rodape">
+                    <p>&copy;2023 UAIBook. Todos os direitos reservados.</p>
+                    <p>Curso de Desenvolvimento em Sistemas.Trilhas do Futuro II. SENAI. Uberaba/MG.</p>
+                </div>
+            </footer>
 </body>
+
 
 </html>
