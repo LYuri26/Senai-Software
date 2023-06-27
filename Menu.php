88297579decf6d89ba0585fff468d359f13cfd0a
JoaoVictor897 <133658734+JoaoVictor897@users.noreply.github.com>
2023-06-27 11:42:54 -0300

    Atualizações e estilizações
    Jvdeamo, Eduardo e João Victor Vendramini
    
    Co-Authored-By: João Victor Martins Deamo <57118631+BatataRaiz@users.noreply.github.com>
    Co-Authored-By: EduardoOliveira01 <106635871+EduardoOliveira01@users.noreply.github.com>

--- Menu.php
+++ Menu.php
@@ -80,44 +80,57 @@ $_SESSION['login_message'] = 'Para cancelar um agendamento, por favor, faça log
   </div>
   <script src="./config/assets/js/menu.js"></script>
   <h1>Bem-vindo ao UAIBook!</h1>
-
+<div class="container" id="conteudo">
   <p>O UAIBook é a melhor ferramenta que você, aluno, pode escolher para obter facilidade e bem-estar ao realizar a reserva de um horário na biblioteca!</p>
   <p>Com o UAIBook, agendar o horário desejado na biblioteca nunca foi tão fácil e livre de complicações. Desfrute de uma experiência tranquila e eficiente ao realizar suas reservas sem preocupações.</p>
   <p id="linhatexto">Nosso objetivo é proporcionar a melhor experiência possível para aqueles que buscam um futuro brilhante através da utilização desse espaço valioso. O site foi desenvolvido com carinho e dedicação pelos talentosos alunos da turma de Desenvolvimento de Sistemas.</p>
+  </div>
   <!-- <script src="./config/assets/js/destruirSessao.js"></script> -->
   <script src="./config/assets/js/destruirSessao.js"></script>
-  <!DOCTYPE html>
-<html lang="pt-br">
 
-<head>
-    <!-- cabeçalho omitido por brevidade -->
-    <style>
-        footer {
-            position: fixed;
-            left: 0;
-            bottom: 0;
-            width: 100%;
-            padding: 10px;
-            text-align: center;
-        }
-    </style>
-</head>
-
-<body>
-    <div id="app">
-        <!-- formulário omitido por brevidade -->
-    </div>
-
-    <footer>
+  <footer>
     <div class="rodape">
-      <strong>&copy; 2023 UAIBook. Todos os direitos reservados.
-      Curso de Desenvolvimento em Sistemas.Trilhas do Futuro II. SENAI. Uberaba/MG.</strong>
+      <p>&copy;2023 UAIBook. Todos os direitos reservados.<br>Curso de Desenvolvimento em Sistemas. Trilhas do
+        Futuro II. <br>SENAI. Uberaba/MG.</p>
     </div>
   </footer>
-
-    <script>
-        // scripts omitidos por brevidade
-    </script>
 </body>
 
 </html>
+<!--
+  <!DOCTYPE html>
+  <html lang="pt-br">
+
+  <head>
+     cabeçalho omitido por brevidade 
+    <style>
+      footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        padding: 10px;
+        text-align: center;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div id="app">
+      formulário omitido por brevidade 
+    </div>
+
+    <footer>
+      <div class="rodape">
+        <strong>&copy; 2023 UAIBook. Todos os direitos reservados.
+          Curso de Desenvolvimento em Sistemas.Trilhas do Futuro II. SENAI. Uberaba/MG.</strong>
+      </div>
+    </footer>
+
+    <script>
+      // scripts omitidos por brevidade
+    </script>
+  </body>
+
+  </html>
+    -->
