ba199f2fcb56331db823aa9507d0990d73c31b06
João Victor Martins Deamo <joaodeamo3@gmail.com>
2023-10-24 11:05:13 -0300

    .

--- config/assets/js/default.js
+++ config/assets/js/default.js
@@ -21,7 +21,9 @@ if (erroParam === '1001') {
   var msgErro = document.getElementById('falseMsg');
   msgErro.textContent = 'Usuário ou senha inválidos';
   msgErro.classList.add('erro'); // Adicionar a classe 'erro' ao elemento
+  
   msgErro.style.visibility = 'visible'; // Exibir o elemento
+  
   msgErro.style.display = 'block'; // Exibir o elemento
   history.replaceState(null, 'Login', 'login.html'); // Redirecionar para a página inicial sem nova requisição
   //window.location.replace('login.html'); // O window location também redireciona para a página inicial, no entanto, ele também atualiza a página, diferentemente do replaceState, que apenas restaura um estado anterior da página
@@ -35,4 +37,3 @@ if (skip_message === '1') {
   window.location.href = 'cancelar.php?skip_message=1';
   history.replaceState(null, 'Cancelar', 'cancelar.php?skip_message=1'); // Redirecionar para a página inicial
 };
-
